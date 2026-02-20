ad3c93e256e46a2a6b2431a3db431a79011fd52f
Lucas Pimentel <lucaspimentel808@gmail.com>
2026-02-20 17:18:27 -0300

    Corpo do projeto

---
+++ src/main/java/me/lucaspmntl/geofreight/GeoFreightApplication.java
@@ -0,0 +1,13 @@
+package me.lucaspmntl.geofreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFreightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoFreightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/lucaspmntl/geofreight/GeoFreightApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucaspmntl.geofreight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoFreightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
