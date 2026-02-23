b55664f0c4660900ab4be6f28d3d99dd3161bcd8
Lucas Santos <Lucaspimentel808@gmail.com>
2026-02-23 18:37:44 -0300

    Entidade Modal e atributos

--- src/main/java/me/lucaspmntl/geofreight/model/Modal.java
+++ src/main/java/me/lucaspmntl/geofreight/model/Modal.java
@@ -1,7 +1,24 @@
 package me.lucaspmntl.geofreight.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class Modal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
 }
