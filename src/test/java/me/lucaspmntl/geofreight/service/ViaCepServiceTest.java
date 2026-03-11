e2967b9bede73e1e83beabe1a9776799988874de
Lucas Santos <Lucaspimentel808@gmail.com>
2026-03-11 14:38:57 -0300

    Commit before push

--- src/test/java/me/lucaspmntl/geofreight/service/ViaCepServiceTest.java
+++ src/test/java/me/lucaspmntl/geofreight/service/ViaCepServiceTest.java
@@ -3,12 +3,15 @@ package me.lucaspmntl.geofreight.service;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.junit.jupiter.api.Assertions.*;
 
 @WireMockTest(httpPort = 8089)
 class ViaCepServiceTest {
 
     @Test
-    void getAddressByCep() {
+    void MustReturnCorrectAddress() {
+        stubFor(get())
     }
 }
