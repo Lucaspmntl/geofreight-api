c59aa41d9acb4f533e0e1f573a161ddbddaaf815
Lucas Santos <Lucaspimentel808@gmail.com>
2026-03-11 21:20:39 -0300

    Freight Orquestrator
    Orquestrator com método de obter endereço utilizando demais API's externas, inclusive fallback

---
+++ src/main/java/me/lucaspmntl/geofreight/service/serviceimpl/FreightOrquestrator.java
@@ -0,0 +1,55 @@
+package me.lucaspmntl.geofreight.service.serviceimpl;
+
+import me.lucaspmntl.geofreight.dto.AddressDTO;
+import me.lucaspmntl.geofreight.dto.brasilapi.BrasilApiDTO;
+import me.lucaspmntl.geofreight.dto.brasilapi.CoordinatesDTO;
+import me.lucaspmntl.geofreight.model.Coordinates;
+import me.lucaspmntl.geofreight.service.BrasilApiService;
+import me.lucaspmntl.geofreight.service.NominatimService;
+import me.lucaspmntl.geofreight.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class FreightOrquestrator {
+
+    @Autowired
+    BrasilApiService brasilApiService;
+
+    @Autowired
+    ViaCepService viaCepService;
+
+    @Autowired
+    NominatimService nominatimService;
+
+    public AddressDTO getAddress(String cep){
+        AddressDTO address = viaCepService.getAddressByCep(cep);
+
+        CoordinatesDTO coordinates = brasilApiService
+                .getCoordinatesByCep(cep)
+                .location()
+                .coordinates();
+
+        if (coordinates.latitude() == null || coordinates.longitude() == null){
+
+            coordinates = nominatimService.getCoordinatesByAddress(cep).getFirst();
+        }
+
+        return new AddressDTO(
+                address.cep(),
+                address.street(),
+                address.complement(),
+                address.unit(),
+                address.neighborhood(),
+                address.city(),
+                address.region(),
+                address.uf(),
+                address.state(),
+                address.ibge(),
+                address.ddd(),
+                address.gia(),
+                address.siafi(),
+                new Coordinates(coordinates.longitude(), coordinates.latitude())
+        );
+    }
+}
