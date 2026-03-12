dbea210e872e91d4993b27f2cbeb03b42adb5bc1
Lucas Santos <Lucaspimentel808@gmail.com>
2026-03-11 21:33:37 -0300

    Classe coordinates

---
+++ src/main/java/me/lucaspmntl/geofreight/model/Coordinates.java
@@ -0,0 +1,16 @@
+package me.lucaspmntl.geofreight.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coordinates {
+    Double latitude;
+    Double longitude;
+}
