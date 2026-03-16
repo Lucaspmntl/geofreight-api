433ed542163f24ff4fd15a9bacb0dd1966e9e039
Lucas Pimentel <lucaspimentel808@gmail.com>
2026-03-16 20:16:44 -0300

    Mudança no endpoint
    api/v1/shippings

--- src/main/java/me/lucaspmntl/geofreight/controller/GeoFreightController.java
+++ src/main/java/me/lucaspmntl/geofreight/controller/GeoFreightController.java
@@ -5,21 +5,21 @@ import me.lucaspmntl.geofreight.dto.GeoFreightResponseDTO;
 import me.lucaspmntl.geofreight.service.serviceimpl.FreightOrquestrator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/geofreight/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class GeoFreightController {
 
     @Autowired
     private FreightOrquestrator freightOrquestrator;
 
-    @PostMapping(value = "freightsOptions/", consumes = "application/json", produces = "application/json")
+    @PostMapping(value = "/shippings", consumes = "application/json", produces = "application/json")
     public ResponseEntity<List<GeoFreightResponseDTO>> getFreightsOptions(
             @RequestBody GeoFreightRequestDTO geoFreightRequestDTO) {
 
