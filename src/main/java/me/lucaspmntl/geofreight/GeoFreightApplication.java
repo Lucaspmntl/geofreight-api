798fb6f879d059a3070ecb3eeb25911405adf2e7
Lucas Pimentel <lucaspimentel808@gmail.com>
2026-03-18 18:28:59 -0300

    Adição de OpenAPI
    Documentação clara e prática

--- src/main/java/me/lucaspmntl/geofreight/GeoFreightApplication.java
+++ src/main/java/me/lucaspmntl/geofreight/GeoFreightApplication.java
@@ -1,11 +1,20 @@
 package me.lucaspmntl.geofreight;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(
+		title = "GeoFreight API",
+		version = "1.0.0",
+		description = "API para cálculo de fretes regionais (Amapá)",
+		contact = @Contact(url = "https://github.com/Lucaspmntl", name = "Lucas Pimentel")
+))
 public class GeoFreightApplication {
 
 	public static void main(String[] args) {
